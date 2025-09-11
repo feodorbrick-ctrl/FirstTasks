8e429b1800750f9de3104a68635cfa76f22d9ba9
Anton Shcherbakov <1mailanton@gmail.com>
2025-09-11 14:25:45 +0200

    if

--- ascendingOrder.js
+++ ascendingOrder.js
@@ -7,28 +7,6 @@
         }
         for (let i = 0; i < str.length; i++) {
                 if (str[i] > str[i + 1]) {
-
-                    deliteElement = str[i + 1]
-                    str[i + 1] = str[i]
-                    str[i] = deliteElement
-                    if (str.indexOf(elementKey[i]) > i + 2) {
-
-                        deliteElement = str[i + 1]
-                        str[i + 1] = str[i]
-                        str[i] = deliteElement
-                        if (str.indexOf(elementKey[i]) > i + 2) {
-
-                            deliteElement = str[i + 1]
-                            str[i + 1] = str[i]
-                            str[i] = deliteElement
-                            if (str.indexOf(elementKey[i]) > i + 2) {
-
-                                deliteElement = str[i + 1]
-                                str[i + 1] = str[i]
-                                str[i] = deliteElement
-                            }
-                        }
-                    }
                 }
         }
         return str
