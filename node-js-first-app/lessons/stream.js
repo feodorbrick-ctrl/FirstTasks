8456fa77f54dd05bd0258f0d7af70bd11278efb5
Anton Shcherbakov <1mailanton@gmail.com>
2026-02-17 09:19:35 +0100

    Write stream and small server

--- node-js-first-app/lessons/stream.js
+++ node-js-first-app/lessons/stream.js
@@ -22,3 +22,19 @@ const path = require('path')
 // stream.on('end', () => console.log('Reading is stoped'))
 // stream.on('open', () => console.log('Reading is starting'))
 // stream.on('error', (e) => console.error(e))
+
+// const writableStream = fs.createWriteStream(path.resolve(__dirname, 'text2.txt'))
+// for (let i = 0; i < 21; i++) {
+//     writableStream.write(i + '\n');
+// }
+// writableStream.end()
+// writableStream.close()
+// writableStream.destroy()
+// writableStream.on('error')
+
+// const http = require('http')
+//
+// http.createServer((req, res) => {
+//     const stream = fs.createReadStream(path.resolve(__dirname, 'test.txt'))
+//     stream.pipe(res)
+// })
