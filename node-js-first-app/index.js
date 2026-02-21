ee6fb3fb71f685035417cd7d15414f8448752e32
Anton Shcherbakov <1mailanton@gmail.com>
2026-02-21 16:01:39 +0100

    work with server

--- node-js-first-app/index.js
+++ node-js-first-app/index.js
@@ -1,10 +1,66 @@
 const http = require('http')
+const eventEmitter = require('events')
+const {handle} = require("express/lib/application");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+const emitter = new eventEmitter();
+
+class Router {
+    constructor() {
+        this.endpoints = {}
+    }
+
+    #request(method = 'GET', path, handler) {
+        if (!this.endpoints[path]) {
+            this.endpoints[path] = {}
+        }
+        const endpoint = this.endpoints[path]
+
+        if (endpoint[method]) {
+            throw new Error(`[${method} is already in use]`)
+        }
+
+        endpoint[method] = handler
+        emitter.on(`[${path}]:[${method}]`, (req, res) => {
+            handler(req, res)
+        })
+    }
+
+    get(path, handler) {
+        this.#request('GET', path, handler)
+    }
+
+    post(path, handler) {
+        this.#request('Post', path, handler)
+    }
+
+    put(path, handler) {
+        this.#request('Put', path, handler)
+    }
+
+    delete(path, handler) {
+        this.#request('Delete', path, handler)
+    }
+}
+
+const router = new Router()
+
+router.get('/users', (req, res) => {
+    res.end('You send request to /Users')
+})
+
+router.post('/users', (req, res) => {
+    res.end('You send request to /Post')
+})
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end('Server is running!');
+    const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req ,res)
+    if (!emitted) {
+        res.end()
+    }
 });
 
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
